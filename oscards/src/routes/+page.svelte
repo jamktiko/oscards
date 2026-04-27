27dcc55477d0d492ee2891646e0bb0565137cb40
Tuomas Huhtala <ag2929@student.jamk.fi>
2026-04-27 13:18:38 +0300

    Moviecard.svelte ja Poster.svelte aluille, movieGroups.ts tiedostoon sisältö

---
+++ oscards/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import MovieCard from '$lib/MovieCard.svelte';
+	import Poster from '$lib/Poster.svelte';
+	import { movieGroups } from '$lib/data/movieGroups';
+</script>
+
+{#each movieGroups.cillianMurphy as cillu}
+	<MovieCard elokuvaTunnus={cillu.imdbId} />
+{/each}
