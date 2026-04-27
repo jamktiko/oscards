bf72f830281c1df624e7bfb200c4894de9ae52de
Tuomas Huhtala <ag2929@student.jamk.fi>
2026-04-27 10:29:39 +0300

    jsonja tyyppi tiedostot kuntoon

---
+++ oscards/src/lib/tyypit.d.ts
@@ -0,0 +1,19 @@
+export type Movie = {
+	id: number;
+	title: string;
+	year: number;
+	length: number; // minutes
+	imdbRating: number;
+	oscarWins: number;
+	streaming: string[];
+	imdbID: string;
+};
+export type Person = {
+	id: number;
+	name: string;
+	born: number;
+	nationality: string;
+	oscarNominations: number;
+	ocscarWins: number;
+	totalMovies: string;
+};
