fe2fef9c33331963ca41ddce5a93b2a9a59254ce
Joel Hänninen <ag3251@student.jamk.fi>
2026-04-28 15:09:41 +0300

    headerfooter taistelu

--- oscards/src/lib/Footer.svelte
+++ oscards/src/lib/Footer.svelte
@@ -2,7 +2,7 @@
 	import CategoryButton from '../routes/categoryButton.svelte';
 </script>
 
-<footer class="w-full bg-zinc-900 py-6">
+<footer class="w-full py-6">
 	<div class="mx-auto flex max-w-7xl flex-col items-center justify-between gap-4 px-6 md:flex-row">
 		<div class="font-judson text-xl font-bold text-zinc-300 md:text-2xl">
 			Copyright 2026 © OsCards co.
--- oscards/src/lib/Header.svelte
+++ oscards/src/lib/Header.svelte
@@ -2,16 +2,22 @@
 	let { otsikko }: { otsikko: string } = $props();
 </script>
 
-<header class="w-full bg-zinc-900 py-4">
-	<div class="mx-auto flex max-w-screen-xl items-center justify-between gap-6 px-6">
-		<div class="flex h-14 w-14 items-center justify-center rounded-[5px] bg-zinc-900">
-			<span class="material-symbols-outlined text-4xl text-yellow-400"> home </span>
-		</div>
+<header class="w-full py-4">
+	<div class="mx-auto mt-5 mb-7 flex max-w-7xl items-center justify-between gap-6 px-6">
+		<a href="/"
+			><span class="material-symbols-outlined text-7xl text-yellow-400" style="font-size: 3rem"
+				>home</span
+			></a
+		>
 
 		<div class="flex-1 text-center font-['Lora'] text-3xl font-bold text-yellow-400 md:text-5xl">
 			{otsikko}
 		</div>
 
-		<span class="material-symbols-outlined text-6xl text-yellow-400"> favorite </span>
+		<a href="/favorites"
+			><span class="material-symbols-outlined text-7xl text-yellow-400" style="font-size: 3rem">
+				favorite
+			</span></a
+		>
 	</div>
 </header>
