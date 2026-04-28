1d96335531a20c55a06011b1d69a5070596471cb
Joel Hänninen <ag3251@student.jamk.fi>
2026-04-28 09:27:39 +0300

    headeriin tuotu pientä figmasta

--- oscards/src/lib/Footer.svelte
+++ oscards/src/lib/Footer.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 </script>
 
-<div class="m-10 flex flex-row gap-10 bg-gray-800 p-2">
-	<div class="">Copyright 2026 © OsCards co.</div>
-	<a href="/about" class="">Credits</a>
+<div
+	class="inline-flex h-24 w-360 items-start justify-start gap-96 bg-zinc-900 pt-7 pr-14 pb-1.5 pl-137"
+>
+	<div class="font-judson justify-start text-2xl font-bold text-zinc-300">
+		Copyright 2026 © OsCards co.
+	</div>
+	<div class="font-judson justify-start text-2xl font-bold text-zinc-300">Credits</div>
 </div>
--- oscards/src/lib/Header.svelte
+++ oscards/src/lib/Header.svelte
@@ -2,8 +2,12 @@
 	let { otsikko }: { otsikko: string } = $props();
 </script>
 
-<div class="w-100% flex flex-row justify-between gap-2 bg-gray-800 p-7 text-white">
+<div class="w-100% flex flex-row justify-between gap-2 bg-zinc-900 p-7 text-white">
 	<span class="material-symbols-outlined border-2 text-3xl">home</span>
 	<h1 class="font-judson">{otsikko}</h1>
-	<span class="material-symbols-outlined border-2 text-3xl">favorite</span>
+	<div
+		class="inline-flex h-14 w-14 items-start justify-start gap-2.5 rounded-[10px] bg-zinc-900 p-2.5 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] outline outline-yellow-400"
+	>
+		<div class="material-symbols-outlined h-8 w-8 items-center bg-yellow-400">favorite</div>
+	</div>
 </div>
