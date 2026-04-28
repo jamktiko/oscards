d30a53ad51f3bace9b5eb60e90a5f61a90196788
Joel Hänninen <ag3251@student.jamk.fi>
2026-04-28 14:35:33 +0300

    header ja footer muokattu joel

--- oscards/src/lib/Header.svelte
+++ oscards/src/lib/Header.svelte
@@ -2,12 +2,18 @@
 	let { otsikko }: { otsikko: string } = $props();
 </script>
 
-<div class="w-100% flex flex-row justify-between gap-2 bg-zinc-900 p-7 text-white">
-	<span class="material-symbols-outlined border-2 text-3xl">home</span>
-	<h1 class="font-judson">{otsikko}</h1>
-	<div
-		class="inline-flex h-14 w-14 items-start justify-start gap-2.5 rounded-[10px] bg-zinc-900 p-2.5 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] outline outline-yellow-400"
-	>
-		<div class="material-symbols-outlined h-8 w-8 items-center bg-yellow-400">favorite</div>
+<header class="w-full bg-zinc-900 py-4">
+	<div class="mx-auto flex max-w-screen-xl items-center justify-between gap-6 px-6">
+		<!-- VASEN IKONI: HOME -->
+		<div class="flex h-14 w-14 items-center justify-center rounded-[5px] bg-zinc-900">
+			<span class="material-symbols-outlined text-4xl text-yellow-400"> home </span>
+		</div>
+
+		<!-- TEKSTI -->
+		<div class="flex-1 text-center font-['Lora'] text-3xl font-bold text-yellow-400 md:text-5xl">
+			{otsikko}
+		</div>
+
+		<span class="material-symbols-outlined text-6xl text-yellow-400"> favorite </span>
 	</div>
-</div>
+</header>
