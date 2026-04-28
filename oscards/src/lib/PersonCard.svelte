2eba35598850b62da3f661c29e5486b740575fa0
Joel Hänninen <ag3251@student.jamk.fi>
2026-04-28 14:10:57 +0300

    lisää moviecardin asettelua

--- oscards/src/lib/PersonCard.svelte
+++ oscards/src/lib/PersonCard.svelte
@@ -15,7 +15,7 @@
 </script>
 
 <div
-	class="relative mx-auto h-auto w-full max-w-sm overflow-hidden rounded-[10px] bg-zinc-900 shadow-[0_0_35px_rgba(0,0,0,0.55)] outline outline-2 outline-offset-[-2px] outline-yellow-400"
+	class="relative mx-auto h-auto w-full max-w-sm overflow-hidden rounded-[10px] bg-zinc-900 pt-5 pb-10 shadow-[0_0_35px_rgba(0,0,0,0.55)] outline outline-yellow-400"
 >
 	<button class="h-full w-full text-left" {onclick}>
 		<div
@@ -27,25 +27,27 @@
 		<div class="mt-4 px-6 text-center font-['Lora'] text-2xl font-bold text-zinc-300 md:text-3xl">
 			{person?.name}
 		</div>
+		<!-- INFO + OSCARS vierekkäin -->
+		<div class="mt-6 flex flex-col gap-6 px-6 md:flex-row md:items-start md:justify-between">
+			<!-- INFO -->
+			<div class="space-y-2 font-['Lora'] text-lg font-medium text-zinc-300 md:text-xl">
+				<div>Born: {person?.born}</div>
+				<div>Nationality: {person?.nationality}</div>
+				<div>Total movies: {person?.totalMovies}</div>
+				<div>Oscar nominations: {person?.oscarNominations}</div>
+			</div>
 
-		<div class="mt-4 space-y-2 px-6 font-['Lora'] text-lg font-medium text-zinc-300 md:text-xl">
-			<div>Born: {person?.born}</div>
-			<div>Nationality: {person?.nationality}</div>
-			<div>Total movies: {person?.totalMovies}</div>
-			<div>Oscar nominations: {person?.oscarNominations}</div>
+			<!-- OSCARS -->
+			<div
+				class="flex items-center gap-3 font-['Lora'] text-lg font-medium text-zinc-300 md:text-xl"
+			>
+				<span>Oscars won:</span>
+				<img
+					src="/img/oscarPalkinto.png"
+					alt="oscar"
+					class="h-10 w-auto drop-shadow-[0_0_15px_rgba(239,191,4,1)] md:h-12"
+				/>
+			</div>
 		</div>
-
-		<div
-			class="mt-6 flex items-center justify-center gap-3 px-6 font-['Lora'] text-lg font-medium text-zinc-300 md:text-xl"
-		>
-			<span>Oscars won:</span>
-			<img
-				src="/img/oscarPalkinto.png"
-				alt="oscar"
-				class="h-10 w-auto drop-shadow-[0_0_15px_rgba(239,191,4,1)] md:h-12"
-			/>
-		</div>
-
-		<div class="h-6"></div>
 	</button>
 </div>
