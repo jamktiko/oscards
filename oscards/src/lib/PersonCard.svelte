79cf685178bcdaeae809276796cd48b52db9df33
Joel Hänninen <ag3251@student.jamk.fi>
2026-04-28 13:47:20 +0300

    kortti-svelten ui vauhtiin

--- oscards/src/lib/PersonCard.svelte
+++ oscards/src/lib/PersonCard.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import type { Person } from '$lib/tyypit';
+
 	let { nimi, onclick }: { nimi: string; onclick: () => void } = $props();
 	let persons: Person[] = $state([]);
 
 	onMount(async () => {
 		const response = await fetch(`/data/people.json`);
-
 		persons = await response.json();
 	});
 
@@ -14,24 +14,38 @@
 	let kuva = $derived(person?.image);
 </script>
 
-<div>
-	<button {onclick}>
-		<div class="m-10 h-30 w-20">
-			<img src={kuva} alt="" />
+<div
+	class="relative mx-auto h-auto w-full max-w-sm overflow-hidden rounded-[10px] bg-zinc-900 shadow-[0_0_35px_rgba(0,0,0,0.55)] outline outline-2 outline-offset-[-2px] outline-yellow-400"
+>
+	<button class="h-full w-full text-left" {onclick}>
+		<div
+			class="mx-auto mt-4 aspect-square w-[85%] overflow-hidden rounded-[10px] outline-yellow-400"
+		>
+			<img src={kuva} alt="kuva" class="h-auto w-auto object-cover" />
 		</div>
-		<div>
-			<h1>
-				{person?.name}
-			</h1>
+
+		<div class="mt-4 px-6 text-center font-['Lora'] text-2xl font-bold text-zinc-300 md:text-3xl">
+			{person?.name}
 		</div>
-		<div>
-			<p>Born: {person?.born}</p>
-			<p>Nationality: {person?.nationality}</p>
-			<p>Total movies: {person?.totalMovies}</p>
-			<p>Oscar nominations: {person?.oscarNominations}</p>
+
+		<div class="mt-4 space-y-2 px-6 font-['Lora'] text-lg font-medium text-zinc-300 md:text-xl">
+			<div>Born: {person?.born}</div>
+			<div>Nationality: {person?.nationality}</div>
+			<div>Total movies: {person?.totalMovies}</div>
+			<div>Oscar nominations: {person?.oscarNominations}</div>
 		</div>
-		<div>
-			<p>Oscars won:</p>
+
+		<div
+			class="mt-6 flex items-center justify-center gap-3 px-6 font-['Lora'] text-lg font-medium text-zinc-300 md:text-xl"
+		>
+			<span>Oscars won:</span>
+			<img
+				src="/img/oscarPalkinto.png"
+				alt="oscar"
+				class="h-10 w-auto drop-shadow-[0_0_15px_rgba(239,191,4,1)] md:h-12"
+			/>
 		</div>
+
+		<div class="h-6"></div>
 	</button>
 </div>
