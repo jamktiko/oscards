7f675db07b064c8364292a569690ad2fc866105b
Joel Hänninen <ag3251@student.jamk.fi>
2026-04-30 09:47:30 +0300

    kortit keskitetty

--- oscards/src/lib/Carousel.svelte
+++ oscards/src/lib/Carousel.svelte
@@ -38,25 +38,26 @@
 	}
 </script>
 
-{#each cards as setti, i (setti)}
-	{@const diff = getDiff(i)}
-	<button onclick={() => handleClick(diff)}>
-		<div
-			class="absolute left-193 will-change-transform"
-			style={` 
-		transform:
-			translateX(${diff * 260}px)
-			scale(${diff === 0 ? 1 : 0.95});
-		filter: brightness(${diff === 0 ? 1 : 1});
-		filter: blur(${diff === 0 ? 0 : 1.5}px);
-		opacity: ${Math.abs(diff) <= 1 ? 1 : 0};
-		pointer-events: ${Math.abs(diff) <= 1 ? 'auto' : 'none'};
-		z-index: ${100 - Math.abs(diff)};
-		transition: all 0.45s ease;
-		cursor: ${diff !== 0 ? 'pointer' : 'default'};
-	`}
-		>
-			{@render children(setti, diff)}
-		</div>
-	</button>
-{/each}
+<div class="relative flex w-full justify-center">
+	{#each cards as setti, i (setti)}
+		{@const diff = getDiff(i)}
+		<button onclick={() => handleClick(diff)}>
+			<div
+				class="absolute left-1/2 -translate-x-1/2 will-change-transform"
+				style={` 
+        transform:
+            translateX(${diff * 260}px)
+            scale(${diff === 0 ? 1 : 0.95});
+        filter: blur(${diff === 0 ? 0 : 1.5}px);
+        opacity: ${Math.abs(diff) <= 1 ? 1 : 0};
+        pointer-events: ${Math.abs(diff) <= 1 ? 'auto' : 'none'};
+        z-index: ${100 - Math.abs(diff)};
+        transition: all 0.45s ease;
+        cursor: ${diff !== 0 ? 'pointer' : 'default'};
+    `}
+			>
+				{@render children(setti, diff)}
+			</div>
+		</button>
+	{/each}
+</div>
