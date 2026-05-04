08f43a2d4eee168d89535a834b0375a5044cba16
Patrick, Uimonen <ah3932@student.jamk.fi>
2026-05-04 11:05:05 +0300

    favoriteihin modal

--- oscards/src/routes/favorites/+page.svelte
+++ oscards/src/routes/favorites/+page.svelte
@@ -3,18 +3,71 @@
 	import { favorite } from '$lib/favorites.svelte';
 	import Header from '$lib/Header.svelte';
 	import Poster from '$lib/Poster.svelte';
+	import Modal from "$lib/Modal.svelte";
+	import { onMount } from 'svelte';
+	import type { Movie } from '$lib/tyypit';
+
+	let showCard=$state(false);
+	let selectedMovie = $state<string | null>(null);
+	let movies: Movie[] = $state([]);
+
+	onMount(async () => {
+		const response = await fetch('/data/movies.json');
+		movies = await response.json();
+	});
+
+	let movie = $derived(movies.find((m) => m.imdbID === selectedMovie));
+
+	function runTime(minutes: number): string {
+		const hours = Math.floor(minutes / 60);
+		const mins = minutes % 60;
+		return `${hours}h ${mins}min`;
+	}
 </script>
 
 <Header otsikko="Favorites" />
 
 <Carousel kortit={favorite.fav}>
-	{#snippet children(setti)}
-		<div class="w-100">
+	{#snippet children(setti, diff)}
+	<!-- svelte-ignore a11y_click_events_have_key_events -->
+	<!-- svelte-ignore a11y_no_static_element_interactions -->
+		<div class="w-100" 
+			onclick={diff === 0 ? () => {showCard = true; selectedMovie = setti} : undefined}> 
 			<Poster elokuvaTunnus={setti} />
 		</div>
+
 		<button
 			class="m-1 self-center rounded-3xl border-2 border-yellow-400 p-4 text-yellow-400"
 			onclick={() => (favorite.unfav = setti)}>Remove</button
 		>
 	{/snippet}
 </Carousel>
+
+{#if showCard}
+	<Modal>
+		{#snippet header()}
+			<button class="material-symbols-outlined ml-[4%] scale-200 text-5xl text-yellow-400 hover:cursor-pointer" onclick={() => showCard = false}> arrow_back</button>
+		{/snippet}
+
+		<div class="flex flex-row items-center gap-1 pr-2 font-judson text-2xl text-zinc-300">
+			<img src="/img/oscarPalkinto.png" alt="" class="h-6 w-6" />
+			x {movie?.oscarWins}
+		</div>
+
+	<div class="flex flex-col items-center font-judson text-zinc-300">
+		<h2 class="text-2xl font-bold">{movie?.title}</h2>
+		<p class="text-lg">Year: {movie?.year}</p>
+		<p class="text-lg">{runTime(movie?.length ?? 0)}</p>
+		<p class="text-lg">IMDb rating: {movie?.imdbRating}</p>
+	</div>
+
+	<div class="flex flex-col items-center gap-2">
+		<h2 class="font-judson text-2xl font-bold text-zinc-300">Streaming on:</h2>
+		<div class="flex gap-3">
+			{#each movie?.streaming as streamin (streamin)}
+				<img src={streamin} alt="" class="h-6 w-6" />
+			{/each}
+		</div>
+	</div>
+	</Modal>
+{/if}
