96a9630462fad426577b23344a328e4ceb646e7d
Nathalie, Penttinen <ah4254@student.jamk.fi>
2026-05-04 10:34:04 +0300

    credits sivun dropdown muokattu

--- oscards/src/routes/about/+page.svelte
+++ oscards/src/routes/about/+page.svelte
@@ -13,7 +13,7 @@
 <div class="flex justify-center p-4">
 	<!-- Kiinteä leveys desktopissa (max-w-md), joustava mobiilissa -->
 	<div
-		class="w-full max-w-md rounded-lg border-4 border-yellow-400 bg-black p-6 text-center text-white shadow-2xl"
+		class="bg-black-900 w-full max-w-md rounded-lg border-4 border-yellow-400 p-6 text-center text-white shadow-2xl"
 	>
 		<!-- OSASYSTEEMI: Tekijät -->
 		<div class="mb-4">
@@ -23,11 +23,11 @@
 				on:click={() => toggle('tekijat')}
 				class="w-full py-2 text-xl font-bold transition hover:text-yellow-400 focus:outline-none"
 			>
-				Tekijät {avoinOsio === 'tekijat' ? '▲' : '▼'}
+				Creators {avoinOsio === 'tekijat' ? '▲' : '▼'}
 			</button>
 
 			{#if avoinOsio === 'tekijat'}
-				<div class="mt-2 space-y-1 rounded bg-gray-900 p-3">
+				<div class="bg-black-900 mt-2 space-y-1 rounded p-3">
 					<p>Tuomas Huhtala</p>
 					<p>Joel Hänninen</p>
 					<p>Nathalie Penttinen</p>
@@ -40,35 +40,103 @@
 		<div class="mb-4">
 			<button
 				type="button"
-				on:click={() => toggle('nayttelijat')}
+				on:click={() => toggle('actors')}
 				class="w-full py-2 text-xl font-bold transition hover:text-yellow-400 focus:outline-none"
 			>
-				Näyttelijät {avoinOsio === 'nayttelijat' ? '▲' : '▼'}
+				Actors/Actresses {avoinOsio === 'actors' ? '▲' : '▼'}
 			</button>
 
-			{#if avoinOsio === 'nayttelijat'}
-				<div class="mt-2 flex flex-col space-y-1 rounded bg-gray-900 p-3">
-					<p>Matthew McConaughey</p>
-					<p>Emma Stone</p>
-					<p>Leonardo DiCaprio</p>
+			{#if avoinOsio === 'actors'}
+				<div class="bg-black-900 mt-2 flex flex-col space-y-1 rounded p-3">
+					<a
+						href="https://commons.wikimedia.org/wiki/File:Matthew_McConaughey_2019_(48648344772).jpg"
+						target="_blank"
+						class="text-white transition hover:text-yellow-400 hover:underline"
+						>Matthew McConaughey</a
+					>
+					<a
+						href="https://commons.wikimedia.org/wiki/File:Emma_Stone_at_the_2024_New_York_Film_Festival_1_(cropped).jpg"
+						target="_blank"
+						class="text-white transition hover:text-yellow-400 hover:underline">Emma Stone</a
+					>
+					<a
+						href="https://commons.wikimedia.org/wiki/File:Leonardo_DiCaprio_2014.jpg"
+						target="_blank"
+						class="text-white transition hover:text-yellow-400 hover:underline">Leonardo Dicaprio</a
+					>
+					<a
+						href="https://commons.wikimedia.org/wiki/File:Natalie_Portman_(48470988352)_(cropped).jpg"
+						target="_blank"
+						class="text-white transition hover:text-yellow-400 hover:underline">Natalie Portman</a
+					>
+					<a
+						href="https://commons.wikimedia.org/wiki/File:CillianMurphy-TIFF2025-01-Cropped_(cropped).png"
+						target="_blank"
+						class="text-white transition hover:text-yellow-400 hover:underline">Cillian Murphy</a
+					>
 				</div>
 			{/if}
 		</div>
+		<button
+			type="button"
+			on:click={() => toggle('directors')}
+			class="w-full py-2 text-xl font-bold transition hover:text-yellow-400 focus:outline-none"
+		>
+			Directors {avoinOsio === 'directors' ? '▲' : '▼'}
+		</button>
 
+		{#if avoinOsio === 'directors'}
+			<div class="bg-black-900 mt-2 flex flex-col space-y-1 rounded p-3">
+				<a
+					href="https://commons.wikimedia.org/wiki/File:Martin_Scorsese-68749.jpg"
+					target="_blank"
+					class="text-white transition hover:text-yellow-400 hover:underline">Martin Scorsese</a
+				>
+				<a
+					href="https://commons.wikimedia.org/wiki/File:Christopher_Nolan,_London,_2013_(crop).jpg"
+					target="_blank"
+					class="text-white transition hover:text-yellow-400 hover:underline">Christopher Nolan</a
+				>
+				<a
+					href="https://commons.wikimedia.org/wiki/File:Paul_Thomas_Anderson_-_BFI_Southbank.jpg"
+					target="_blank"
+					class="text-white transition hover:text-yellow-400 hover:underline"
+					>Paul Thomas Anderson</a
+				>
+				<a
+					href="https://commons.wikimedia.org/wiki/File:James_Cameron_(28003289954)_Cropped.png"
+					target="_blank"
+					class="text-white transition hover:text-yellow-400 hover:underline">James Cameron</a
+				>
+				<a
+					href="https://commons.wikimedia.org/wiki/File:Jane_Campion_DNZM_(cropped).jpg"
+					target="_blank"
+					class="text-white transition hover:text-yellow-400 hover:underline">Jane Campion</a
+				>
+			</div>
+		{/if}
 		<!-- OSASYSTEEMI: Extra -->
 		<div class="mb-4">
 			<button
 				type="button"
 				on:click={() => toggle('extra')}
-				class="w-full py-2 text-xl font-bold transition hover:text-yellow-400 focus:outline-none"
+				class="mt-2 w-full py-2 text-xl font-bold transition hover:text-yellow-400 focus:outline-none"
 			>
-				Extra {avoinOsio === 'extra' ? '▲' : '▼'}
+				Web pages {avoinOsio === 'extra' ? '▲' : '▼'}
 			</button>
 
 			{#if avoinOsio === 'extra'}
-				<div class="mt-2 flex flex-col space-y-2 rounded bg-gray-900 p-3 text-blue-400">
-					<a href="https://imdb-api.com" target="_blank" class="hover:underline">API Key</a>
-					<a href="https://wikimedia.org" target="_blank" class="hover:underline">Wikimedia.org</a>
+				<div class="bg-black-900 mt-2 flex flex-col space-y-2 rounded p-3">
+					<a
+						href="https://imdb-api.com"
+						target="_blank"
+						class="text-white transition hover:text-yellow-400 hover:underline">API Key</a
+					>
+					<a
+						href="https://wikimedia.org"
+						target="_blank"
+						class="text-white transition hover:text-yellow-400 hover:underline">Wikimedia.org</a
+					>
 				</div>
 			{/if}
 		</div>
