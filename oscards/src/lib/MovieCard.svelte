0d44e897e69fc31f32e7eed4b8ff92c37b626265
Joel Hänninen <ag3251@student.jamk.fi>
2026-05-06 10:21:59 +0300

    favoriteviesti

--- oscards/src/lib/MovieCard.svelte
+++ oscards/src/lib/MovieCard.svelte
@@ -24,24 +24,26 @@
 <div
 	class="relative mx-auto h-172.5 w-full max-w-sm gap-4 rounded-xl border-2 border-yellow-400 bg-zinc-900 p-4 shadow-[0_0_35px_rgba(0,0,0,0.55)]"
 >
-<button
-    class="material-symbols-outlined absolute top-7 right-7 scale-175 cursor-pointer text-yellow-400 [font-variation-settings:'FILL'_{favorite.fav.includes(elokuvaTunnus) ? 1 : 0}] hover:text-yellow-400"
-    onclick={() => {
-        if (favorite.fav.includes(elokuvaTunnus)) {
-            favorite.unfav = elokuvaTunnus;
-        } else {
-            favorite.fav = elokuvaTunnus;
-        }
-        favoriteMessage.set(
-            favorite.fav.includes(elokuvaTunnus)
-                ? 'Removed from favorites'
-                : 'Added to favorites'
-        );
-        modalVisible.set(!$modalVisible);
-    }}
->
-    favorite
-</button>
+	<button
+		class="material-symbols-outlined absolute top-7 right-7 scale-175 cursor-pointer text-yellow-400 [font-variation-settings:'FILL'_{favorite.fav.includes(
+			elokuvaTunnus
+		)
+			? 0
+			: 1}] hover:text-yellow-400"
+		onclick={() => {
+			if (favorite.fav.includes(elokuvaTunnus)) {
+				favorite.unfav = elokuvaTunnus;
+			} else {
+				favorite.fav = elokuvaTunnus;
+			}
+			favoriteMessage.set(
+				favorite.fav.includes(elokuvaTunnus) ? 'Added to favorites' : 'Removed from favorites'
+			);
+			modalVisible.set(!$modalVisible);
+		}}
+	>
+		favorite
+	</button>
 
 	<div class="flex h-auto w-auto items-center justify-between">
 		<div class="h-83 w-56 rounded-lg border-2 border-yellow-400 object-cover">
