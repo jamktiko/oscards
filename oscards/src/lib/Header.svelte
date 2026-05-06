3893bb3fb09f42cce8be49e37f74acf30864ead7
Nathalie, Penttinen <ah4254@student.jamk.fi>
2026-05-06 14:19:17 +0300

    tyylittelin nuolen

--- oscards/src/lib/Header.svelte
+++ oscards/src/lib/Header.svelte
@@ -6,23 +6,25 @@
 </script>
 
 <header class="w-full py-8">
-	<div class="mx-auto flex max-w-7xl items-center justify-between gap-6 px-6">
-		{#if ehto}
-			<button
-				class="material-symbols-outlined ml-[4%] scale-200 text-5xl text-yellow-400 hover:cursor-pointer"
-				{onclick}
-			>
-				arrow_back_ios
-			</button>
-		{/if}
-		<!-- Left icon -->
-		<a href={resolve('/')} class="flex h-14 w-14 items-center justify-center">
-			<span
-				class="material-symbols-outlined scale-250 text-yellow-400 drop-shadow-[0_6px_3px_rgba(0,0,0,1)] transition-transform hover:scale-300"
-			>
-				home
-			</span>
-		</a>
+	<div class=" mx-auto flex max-w-7xl items-center justify-between px-6">
+		<div class="flex items-center gap-10">
+			{#if ehto}
+				<button
+					class="material-symbols-outlined ml-[4%] scale-200 text-5xl text-yellow-400 drop-shadow-[0_6px_3px_rgba(0,0,0,1)] transition-transform hover:scale-300 hover:cursor-pointer"
+					{onclick}
+				>
+					arrow_back_ios
+				</button>
+			{/if}
+			<!-- Left icon -->
+			<a href={resolve('/')} class="flex h-14 w-14 items-center justify-center gap-8">
+				<span
+					class="material-symbols-outlined scale-250 text-yellow-400 drop-shadow-[0_6px_3px_rgba(0,0,0,1)] transition-transform hover:scale-300"
+				>
+					home
+				</span>
+			</a>
+		</div>
 
 		<img src={`/img/${otsikko}.png`} alt={otsikko} class="h-auto w-44 md:w-52" />
 
